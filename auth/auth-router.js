18af0416e835172c5934947bd7b7d9db6630bc07
Maddy <mmcintosh2k14@gmail.com>
2019-12-18 13:33:32 -0500

    adds restriced middleware to verify token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -6,56 +6,56 @@ const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-  user.password = hash;
+	let user = req.body;
+	const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+	user.password = hash;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+	Users.add(user)
+		.then(saved => {
+			res.status(201).json(saved);
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
 });
 
 router.post('/login', (req, res) => {
-  let { username, password } = req.body;
+	let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        //sign token
-        const token = signToken(user);
+	Users.findBy({ username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				//sign token
+				const token = signToken(user);
 
-        // send the token
-        res.status(200).json({
-          token,
-          message: `Welcome ${user.username}!`,
-        });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+				// send the token
+				res.status(200).json({
+					token, // will return the token to the client in the response. they can then set it to local storage
+					message: `Welcome ${user.username}!`
+				});
+			} else {
+				res.status(401).json({ message: 'Invalid Credentials' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		});
 });
 
+// this function creates and signs the token
 function signToken(user) {
-  const payload = {
-    username: user.username
-  };
+	const payload = {
+		username: user.username
+	};
 
-  const secret = process.env.JWT_SECRET || 'pinky promise, please!';
+	const secret = process.env.JWT_SECRET || 'pinky promise, please!';
 
-  const options = {
-    expiresIn: "1hr",
+	const options = {
+		expiresIn: '1hr'
+	};
 
-  }
-  
-  jwt.sign(payload, secret, options)
+	return jwt.sign(payload, secret, options);
 }
 
 module.exports = router;
