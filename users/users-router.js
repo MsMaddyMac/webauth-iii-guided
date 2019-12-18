d72e222e599077773128dcdbc7a147d757772dd3
Maddy <mmcintosh2k14@gmail.com>
2019-12-18 14:10:06 -0500

    adds notes

--- users/users-router.js
+++ users/users-router.js
@@ -4,11 +4,21 @@ const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
 
 router.get('/', restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
+	Users.find()
+		.then(users => {
+			res.json(users);
+		})
+		.catch(err => res.send(err));
 });
 
+// if you are wanting to check role too you would create this middleware in another file and add it as a parameter above
+// function checkRole(role) {
+// 	return function(req, res, next) {
+// 		if (req.token && role === req.token.role) {
+// 			next();
+// 		} else {
+// 			res.status(403).json({ message: 'You have no power here, you must be a ${role}.' });
+// 		}
+// 	};
+// }
 module.exports = router;
